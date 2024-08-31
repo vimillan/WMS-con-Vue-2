d226822e2ae27f0b646bffbdbb000fb3cb476cba
vimillan <vimillanvi@gmail.com>
2024-08-30 22:17:23 -0600

    style: Deleted useless classes

--- src/components/WarehouseStats.vue
+++ src/components/WarehouseStats.vue
@@ -2,7 +2,7 @@
     <div class="center-item gap-3 mb-3">
         <div class="my-2 center-item gap-2 justify-content-between">
             <div class="bg-white shadow rounded-xs center-item gap-3" style="padding: 10px 17px;">
-                <div class="sideborder-black ps-2">
+                <div class="ps-2">
                     <small class="text-secondary fs-11">Total productos</small>
                     <div class="fs-20 fw-bold">
                         {{ stats.productos }}
@@ -15,7 +15,7 @@
         </div>
         <div class="my-3 center-item gap-2 justify-content-between">
             <div class="bg-white shadow rounded-xs center-item gap-4" style="padding: 10px 17px;">
-                <div class="sideborder-black ps-2">
+                <div class="ps-2">
                     <small class="text-secondary fs-11">Total productos</small>
                     <div class="fs-20 fw-bold">
                         {{ stats.almacen }}
